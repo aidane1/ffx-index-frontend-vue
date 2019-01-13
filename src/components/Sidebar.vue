335c95c2ccd555691db4981d1993388a9c758dd8
Kieran Eglin <kieran.eglin@gmail.com>
2019-01-13 15:13:45 -0800

    added sidebar arrows

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,9 +2,23 @@
 aside.menu
   .sidebar__toggle-button(@click='toggleSidebar')
     i.fas.fa-bars
-  ul.menu-list
-    li(v-for='section in sections', :key='section.label')
-      a {{ section.label }}
+  .menu-list__container
+    ul.menu-list.menu-list-spaced
+      li(
+        v-for='section in sections', 
+        :key='section.label', 
+        @click='section.expanded = !section.expanded'
+      )
+        a {{ section.label }}
+          span(:class='sectionToggleClass(section)')
+        ul(v-if='section.expanded')
+          li
+            a Members
+          li
+            a Plugins
+          li
+            a Add a member
+
 </template>
 <script>
 import sidebar from './sidebar'
@@ -20,6 +34,13 @@ export default {
     toggleSidebar() {
       this.expanded = !this.expanded
       this.$emit('toggle-sidebar')
+    },
+    sectionToggleClass(section) {
+      if (section.expanded) {
+        return 'fas fa-chevron-down'
+      }
+
+      return 'fas fa-chevron-right'
     }
   }
 }
